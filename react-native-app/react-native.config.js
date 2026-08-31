66115d9dc879ddf9e3c47168ef1662d8dd252e9b
allahforgiveness786-arch <allahforgiveness786@gmail.com>
2026-08-31 23:32:47 +0530

    chore(ci): update build config and clean patches
    Remove legacy react-native-screens patches and simplify react-native.config.js for Android.

--- react-native-app/react-native.config.js
+++ react-native-app/react-native.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   project: {
     ios: {},
-    android: {
-      packageName: 'com.autopartsindia',
-      sourceDir: './android',
-      appName: 'app',
-    },
+    android: {},
   },
 };
